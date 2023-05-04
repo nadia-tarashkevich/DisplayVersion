4b9987b4ca7161ff800bf3b2c04c1a747353dd68
Nadia Tarashkevich <nadia.tarashkevich@jetbrains.com>
2023-05-03 20:34:44 -0700

    Fixed position

--- src/main/java/org/jetbrains/support/DisplayVersion.java
+++ src/main/java/org/jetbrains/support/DisplayVersion.java
@@ -3,10 +3,10 @@ package org.jetbrains.support;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.Presentation;
 import com.intellij.openapi.application.ApplicationInfo;
-import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.editor.colors.EditorColorsManager;
-import com.intellij.util.ui.UIUtil;
+import com.intellij.openapi.editor.colors.EditorColorsScheme;
+import com.intellij.openapi.editor.colors.EditorFontType;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
@@ -34,28 +34,37 @@ public class DisplayVersion extends com.intellij.openapi.actionSystem.AnAction{
         String build = appInfo.getBuild().asString();
         String iconText = version + " " + build;
 
-        int iconHeight = 16;
-        int maxImageWidth = 200;
-        int horizontalGap = 2;
 
-        Font font = new Font("Arial", Font.PLAIN, 6);
+        // set up font
+        EditorColorsScheme colorsScheme = EditorColorsManager.getInstance().getGlobalScheme();
+        String fontName = colorsScheme.getFont(EditorFontType.PLAIN).getFontName();
+        Font font = new Font(fontName, Font.PLAIN, 12);
+
+        // font color
         Color textColor = EditorColorsManager.getInstance().getGlobalScheme().getDefaultForeground();
         Color bgColor = EditorColorsManager.getInstance().getGlobalScheme().getDefaultBackground();
-        BufferedImage image = UIUtil.createImage(maxImageWidth, iconHeight, BufferedImage.TYPE_INT_ARGB);
-        Graphics2D g = image.createGraphics();
+
+        // icon size
+        int iconHeight = 16;
+        int maxIconWidth = 200;
+        int horizontalGap = 2;
+
+
+        GraphicsConfiguration gc = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDefaultConfiguration();
+        BufferedImage bufferedImage = gc.createCompatibleImage(maxIconWidth, iconHeight);
+        Graphics2D g = bufferedImage.createGraphics();
         g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
         FontMetrics metrics = g.getFontMetrics(font);
-        int iconWidth = (metrics.stringWidth(iconText) + horizontalGap * 2) * 2;// 2x default scaling for TYPE_INT_ARGB images
+        int iconWidth = metrics.stringWidth(iconText) + horizontalGap * 2;
 
+        // draw the icon
         g.setColor(bgColor);
         g.fillRect(0, 0, iconWidth, iconHeight);
         g.setFont(font);
         g.setColor(textColor);
-
         g.drawString(iconText, horizontalGap, font.getSize());
         g.dispose();
 
-        ImageIcon imageIcon = new ImageIcon(image.getSubimage(0, 0, iconWidth, iconHeight));
-        return imageIcon;
+        return new ImageIcon(bufferedImage.getSubimage(0, 0, iconWidth, iconHeight));
     }
 }
